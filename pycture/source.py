678a8eadfb8fbf801138661900829fca05a783cc
Alexander <da070116@gmail.com>
2020-06-04 17:17:46 +0300

    start draw and add required dependencies

--- pycture/source.py
+++ pycture/source.py
@@ -1,5 +1,17 @@
 from graph import *
 
+
 def draw_angry_smile():
     """ Function draws an angry smile using standard primitives"""
     print('once there will be a picture')
+
+
+def test():
+    penColor(255,0,255)
+    penSize(5)
+    brushColor("blue")
+    rectangle(100, 100, 300, 200)
+    display = mainWindow()
+
+if __name__ == '__main__':
+    test()
--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='pycture',
       author_email='ad@example.com',
       license='MIT',
       dependency_links=['http://cs.mipt.ru/python/extra/lab3/graph.py'],
-      install_requires=['graph'],
+      install_requires=['graph', 'tk'],
       packages=['pycture'],
       zip_safe=False)
