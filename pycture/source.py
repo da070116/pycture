85190d2bd3ea9a6d465f4a04300a2b67a340ecb3
Alexander <da070116@gmail.com>
2020-06-04 16:46:21 +0300

    Add sources

---
+++ pycture/source.py
@@ -0,0 +1,5 @@
+from graph import *
+
+def draw_angry_smile():
+    """ Function draws an angry smile using standard primitives"""
+    print('once there will be a picture')
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pycture',
+      version='0.1',
+      description='A test graphic application',
+      url='https://github.com/da070116/pycture',
+      author='AD',
+      author_email='ad@example.com',
+      license='MIT',
+      dependency_links=['http://cs.mipt.ru/python/extra/lab3/graph.py'],
+      install_requires=['graph'],
+      packages=['pycture'],
+      zip_safe=False)
