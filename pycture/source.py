0e472067ed9ca055c4784d741ce85660db7d7d4c
Alexander Dubrovin <da070116@gmail.com>
2020-06-05 08:45:40 +0300

    Drawing triangles

--- pycture/source.py
+++ pycture/source.py
@@ -1,4 +1,4 @@
-from graph import *
+from pycture.graph import *
 
 
 def draw_angry_smile():
@@ -11,7 +11,25 @@ def test():
     penSize(5)
     brushColor("blue")
     rectangle(100, 100, 300, 200)
-    display = mainWindow()
+    
+
+
+def draw_triangle(x,y, /, *, width: int, height: int,  color: tuple, border: int = 0):
+    """ Draw a triangle at point x, y """
+    penSize(border)
+    if border == 0 and color == (255,255,255):
+        penSize(1)
+        penColor(0, 0, 0)
+    points_list = [(x, y), (x, y+height), (x+width, y+height), (x, y)]
+    brushColor(color)
+    polygon(points_list)
+
 
 if __name__ == '__main__':
-    test()
+    brushColor(255, 225, 0)
+    penSize(0)
+    rectangle(0, 0, 220, 420)
+    draw_triangle(10, 10, width=100, height=200, color=(0, 0, 0), border=3)
+    draw_triangle(110, 10, width=100, height=200, color=(0, 0, 0), border=3)
+    draw_triangle(110, 210, width=100, height=200, color=(0, 0, 0), border=3)
+    run()
